e9e29326489b4ab1e409684f0b14c12bfa88518a
Victor Jucosky <99966762+vjucosky@users.noreply.github.com>
2025-02-25 08:15:24 -0300

    Add TrustServerCertificate argument

--- etl/settings.py
+++ etl/settings.py
@@ -3,7 +3,7 @@ from os import getenv
 
 
 DATABASE_SETTINGS = {
-    'url': f'mssql+pyodbc://{getenv("DATABASE_USERNAME")}:{getenv("DATABASE_PASSWORD")}@localhost:1433/STUDY?driver=ODBC+Driver+18+for+SQL+Server',
+    'url': f'mssql+pyodbc://{getenv("DATABASE_USERNAME")}:{getenv("DATABASE_PASSWORD")}@localhost:1433/STUDY?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=Yes',
     'fast_executemany': True
 }
 
